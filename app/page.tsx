078dbc933d03833e3c3bc8b93beb61699ab83e66
David <p5y4@laposte.net>
2025-11-19 15:15:58 +0100

    introducing template

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,10 @@
-"use client"
-
 import Header from "@/components/layout/Header";
-import { useDeviceStore } from "./lib/store/useDeviceStore";
-import { useEffect } from "react";
 import Services from "@/components/layout/Services";
 import NextTest from "@/components/layout/NextTest";
 
 export default function Home() {
 
-	const { detectDevice } = useDeviceStore();
-
-	useEffect(() => {
-		const cleanup = detectDevice();
-		return cleanup; 
-	}, [detectDevice]);
+	
 
 	return (
 		<main className="overflow-x-hidden">
