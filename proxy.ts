8cb67414b69ac4e4151b1a215228369ac45c5752
David <p5y4@laposte.net>
2025-12-14 00:35:08 +0100

    secure

--- proxy.ts
+++ proxy.ts
@@ -3,7 +3,14 @@ import { NextResponse } from 'next/server';
 
 
 
-export default clerkMiddleware({
-  authorizedParties: ['https://dvweb-agency.fr'],
-})
+export default clerkMiddleware(
+  (auth, req) => {
+    console.log('=========================')
+    console.log('Request Headers')
+    console.log('x-forwarded-host', req.headers.get('x-forwarded-host'))
+    console.log('x-forwarded-proto', req.headers.get('x-forwarded-proto'))
+    console.log('=========================')
+  },
+  { debug: debugStatus },
+)
 
