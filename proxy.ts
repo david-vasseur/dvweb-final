97b99f321171d0db25e7d4e5456b71503aa1bff7
David <p5y4@laposte.net>
2025-12-13 17:31:58 +0100

    secure

--- proxy.ts
+++ proxy.ts
@@ -1,13 +1,12 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
+import { clerkMiddleware } from '@clerk/nextjs/server'
 
-export default clerkMiddleware({
-  // Si tu veux protéger toutes les routes, laisse vide
-  // Pour protéger certaines routes seulement :
-  // matcher: ["/dashboard/:path*"]
-});
+export default clerkMiddleware()
 
-// ⚠️ C’est nécessaire pour Next.js App Router
 export const config = {
-  matcher: "/((?!_next/static|_next/image|favicon.ico).*)",
-};
+	matcher: [
+		// Skip Next.js internals and all static files, unless found in search params
+		'/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+		// Always run for API routes
+		'/(api|trpc)(.*)',
+	],
+}
