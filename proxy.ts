e7e31418bf31c6de463f308a8d49aa6e40c36094
David <p5y4@laposte.net>
2025-12-13 18:14:27 +0100

    secure

--- proxy.ts
+++ proxy.ts
@@ -1,12 +1,22 @@
-import { clerkMiddleware } from '@clerk/nextjs/server'
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
 
-export default clerkMiddleware()
+const isProtectedRoute = createRouteMatcher([
+  '/dashboard(.*)',
+])
+
+export default clerkMiddleware(async (auth, request) => {
+	const { userId } = await auth()
+
+  if (isProtectedRoute(request) && !userId) {
+    return NextResponse.redirect("/")
+  }
+  return NextResponse.next();
+})
 
 export const config = {
-	matcher: [
-		// Skip Next.js internals and all static files, unless found in search params
-		'/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-		// Always run for API routes
-		'/(api|trpc)(.*)',
-	],
+  matcher: [
+    '/dashboard(.*)',
+    '/api/(.*)',
+  ],
 }
